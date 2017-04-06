bd6e0976e84b38f522b3fcf3480110d33ec677d0
Ondřej Ježil <firma.ondra@gmail.com>
2017-04-06 21:59:34 +0200

    Finishing up first version.

--- ebj.js
+++ ebj.js
@@ -10,40 +10,60 @@ function getFiles(filen){
 	file=files[filen];
 	return( execSync("find "+paths[file.p]+" -name \"*"+file.e+"\"")+"").split(nl);
 }
+
 function getIOPairs(filen){
+	if(filen==null)
+		return [[null,null,null]];
 	file=files[filen];
 	fileo=files[file.o];
 	var arr = (execSync("find "+paths[file.p]+" -name \"*"+file.e+"\"")+"").split(nl);
 	var rarr=[];
 	for(var i=0;i<arr.length-1;i++){
 		var str = arr[i];
-		console.log(str);
 		str = str.substring(paths[file.p].length,str.length-file.e.length);
-		console.log(str);
-		var strs=paths[fileo.p]+str; 
-		str=strs+fileo.e;
-		console.log(str);			
+		var strs=paths[fileo.p];
+		str=strs+str+fileo.e;
 		rarr[i]=[arr[i],str,strs];
 	}
 	return rarr;
 }
-for(filen in files){
-	console.log(getIOPairs(filen));
-}
-console.log("entering main loop");
-for(var i=0;i<cmds.length;i++){
-	var cmd = cmds[i];
-	var filen = cmd[1];
-	var pairs = getIOPairs(filen);
-	for(var j=0;j<pairs.length;j++){
-		var filena = (pairs[j][0]+"").split("/");
-		var ifilenfl = filena[filena.length-1];
-		console.log("ifilenfl: "+ifilenfl);
-		console.log(execSync(cmd[0]
-			.replace("__od",pairs[j][2])
-			.replace("__in",ifilenfl)
-			.replace("__id",pairs[j][0].substring(0,pairs[j][0].length-1-ifilenfl.length))
-			.replace("__o",pairs[j][1])	
-			.replace("__i",pairs[j][0])));
+function replaceAlls(str){
+	for(var filen in files){
+		str = str.replace("__all"+filen,getFiles(filen).join(" "));
 	}
+	return str;
 }
+function main(){
+	console.log("Starting to build by "+ebjFile);
+	for(var i=0;i<cmds.length;i++){
+		var cmd = cmds[i];
+		var filen = cmd[1];
+		var pairs = getIOPairs(filen);
+		for(var j=0;j<pairs.length;j++){
+			var filena = (pairs[j][0]+"").split("/");
+			var ifilenfl = filena[filena.length-1];
+			try{
+				var cmdstrbase = replaceAlls(cmd[0]);
+				var cmdstr;
+				if(filen!=null){
+					cmdstr = cmdstrbase
+						.replace("__od",pairs[j][2])
+						.replace("__in",ifilenfl)
+						.replace("__id",pairs[j][0].substring(0,pairs[j][0].length-1-ifilenfl.length))
+						.replace("__o",pairs[j][1])
+						.replace("__i",pairs[j][0]);
+				}else{
+					cmdstr=cmdstrbase;
+				}
+				var stdout = execSync(cmdstr)+"";
+				if(stdout!=""&&stdout!=null&&stdout!=nl)
+					console.log(stdout);
+			}catch(err){
+				console.log("An error has occured during building, terminating.");
+				return;
+			}
+		}
+	}
+	console.log("Finished building without errors durning cmds execution.");
+}
+main();
